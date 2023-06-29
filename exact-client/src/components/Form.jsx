f4a13d83d005b03d926216811ab455eaf02e75c2
Aslan Mammadov <mammadovaslan21@gmail.com>
2023-06-29 20:52:15 +0400

    selects refractored

--- exact-client/src/components/Form.jsx
+++ exact-client/src/components/Form.jsx
@@ -1,21 +1,13 @@
 import Dropdown from "./Dropdown";
+import data from "../utils/data.json";
 
-const brands = [
-  "Michelin",
-  "Continental",
-  "Bridgestone",
-  "Pirelli",
-  "Goodyear",
-  "Dunlop",
-  "Yokohama",
-  "Hankook",
-  "Falken",
-  "Toyo",
-];
+const getProperty = (property) => {
+  return [...new Set(data.data.map((item) => item[property].toString()))];
+};
 
-const widthOptions = ["205", "215", "225", "235", "245", "255", "265", "275", "285", "295"];
-
-const aspectRatioOptions = ["30", "35", "40", "45", "50", "55", "60", "65", "70", "75"];
+const brands = getProperty("brand");
+const widthOptions = getProperty("width");
+const aspectRatioOptions = getProperty("aspectRatio");
 
 const Form = () => {
   const submitHandler = (e) => {
@@ -27,7 +19,6 @@ const Form = () => {
       <Dropdown options={brands} placeholder="Select a brand" />
       <Dropdown options={widthOptions} placeholder="Select a width" />
       <Dropdown options={aspectRatioOptions} placeholder="Select an aspect ratio" />
-      {/* Add more Dropdown components for additional selects */}
     </form>
   );
 };
